a42fc27f6a81845f7c5d4c5e7dde1b2cd8462655
Alimoff <sobiriddin@gmail.com>
2022-09-25 09:20:53 +0500

    debugging

---
+++ test_api.py
@@ -0,0 +1,19 @@
+# from pytube import YouTube
+# import os
+
+
+# def download(url):
+#     yt = YouTube(url)
+
+#     a = (
+#         yt.streams.filter(progressive=True, file_extension="mp4")
+#         .order_by("resolution")
+#         .desc()
+#         .first()
+#         .download("video/")
+#     )
+
+#     print(a)
+
+
+# download("https://www.youtube.com/watch?v=NXX-I7N_a0k")
